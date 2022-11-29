dc0db7835573671cbe407abaede82be3b7d2106f
rayhankinan <rayhankinan@gmail.com>
2022-11-29 16:40:13 +0700

    feat: add axios

--- src/controllers/soap-controller.ts
+++ src/controllers/soap-controller.ts
@@ -1,10 +1,12 @@
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
+import { parseString } from "xml2js";
+import axios from "axios";
+
 import { AuthRequest } from "../middlewares/authentication-middleware";
 import { soapConfig } from "../config/soap-config";
-import { parseString } from "xml2js";
 
-interface UpdateSubscription {
+interface UpdateSubscriptionRequest {
     creatorID: number;
     subscriberID: number;
 }
@@ -22,39 +24,67 @@ export class SoapController {
             }
 
             // Parse request body
-            const { creatorID, subscriberID }: UpdateSubscription = req.body;
+            const { creatorID, subscriberID }: UpdateSubscriptionRequest =
+                req.body;
 
-            var url = `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`;
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", url);
-            xhr.setRequestHeader("Content-Type", "text/xml");
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        var response = xhr.responseText;
-                        parseString(response, (err: any, result: any) => {
-                            const json = JSON.stringify(result, null, 4);
-                            console.log(json);
-                            res.status(StatusCodes.CREATED).json({
-                                message: ReasonPhrases.CREATED,
-                            });
-                        });
-                    } else {
-                        res.status(StatusCodes.BAD_REQUEST).json({
-                            message: ReasonPhrases.BAD_REQUEST,
-                        });
+            try {
+                await axios.post(
+                    `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`,
+                    `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <approveSubscribe xmlns="http://service.binotify/">
+                                <arg0 xmlns="">${creatorID}</arg0>
+                                <arg1 xmlns="">${subscriberID}</arg1>
+                            </approveSubscribe>
+                        </Body>
+                    </Envelope>`,
+                    {
+                        headers: {
+                            "Content-Type": "text/xml",
+                        },
                     }
-                }
-            };
-            var data = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-                <Body>
-                    <approveSubscribe xmlns="http://service.binotify/">
-                        <arg0 xmlns="">${creatorID}</arg0>
-                        <arg1 xmlns="">${subscriberID}</arg1>
-                    </approveSubscribe>
-                </Body>
-            </Envelope>`;
-            xhr.send(data);
+                );
+
+                res.status(StatusCodes.CREATED).json({
+                    message: ReasonPhrases.CREATED,
+                });
+            } catch (error) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: ReasonPhrases.BAD_REQUEST,
+                });
+            }
+
+            // var url = `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`;
+            // var xhr = new XMLHttpRequest();
+            // xhr.open("POST", url);
+            // xhr.setRequestHeader("Content-Type", "text/xml");
+            // xhr.onreadystatechange = function () {
+            //     if (xhr.readyState === XMLHttpRequest.DONE) {
+            //         if (xhr.status === 200) {
+            //             var response = xhr.responseText;
+            //             parseString(response, (err: any, result: any) => {
+            //                 const json = JSON.stringify(result, null, 4);
+            //                 console.log(json);
+            //                 res.status(StatusCodes.CREATED).json({
+            //                     message: ReasonPhrases.CREATED,
+            //                 });
+            //             });
+            //         } else {
+            //             res.status(StatusCodes.BAD_REQUEST).json({
+            //                 message: ReasonPhrases.BAD_REQUEST,
+            //             });
+            //         }
+            //     }
+            // };
+            // var data = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+            //     <Body>
+            //         <approveSubscribe xmlns="http://service.binotify/">
+            //             <arg0 xmlns="">${creatorID}</arg0>
+            //             <arg1 xmlns="">${subscriberID}</arg1>
+            //         </approveSubscribe>
+            //     </Body>
+            // </Envelope>`;
+            // xhr.send(data);
         };
     }
 
@@ -70,39 +100,67 @@ export class SoapController {
             }
 
             // Parse request body
-            const { creatorID, subscriberID }: UpdateSubscription = req.body;
+            const { creatorID, subscriberID }: UpdateSubscriptionRequest =
+                req.body;
 
-            var url = `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`;
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", url);
-            xhr.setRequestHeader("Content-Type", "text/xml");
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        var response = xhr.responseText;
-                        parseString(response, (err: any, result: any) => {
-                            const json = JSON.stringify(result, null, 4);
-                            console.log(json);
-                            res.status(StatusCodes.CREATED).json({
-                                message: ReasonPhrases.CREATED,
-                            });
-                        });
-                    } else {
-                        res.status(StatusCodes.BAD_REQUEST).json({
-                            message: ReasonPhrases.BAD_REQUEST,
-                        });
+            try {
+                await axios.post(
+                    `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`,
+                    `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                            <rejectSubscribe xmlns="http://service.binotify/">
+                                <arg0 xmlns="">${creatorID}</arg0>
+                                <arg1 xmlns="">${subscriberID}</arg1>
+                            </rejectSubscribe>
+                        </Body>
+                    </Envelope>`,
+                    {
+                        headers: {
+                            "Content-Type": "text/xml",
+                        },
                     }
-                }
-            };
-            var data = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-                <Body>
-                    <rejectSubscribe xmlns="http://service.binotify/">
-                        <arg0 xmlns="">${creatorID}</arg0>
-                        <arg1 xmlns="">${subscriberID}</arg1>
-                    </rejectSubscribe>
-                </Body>
-            </Envelope>`;
-            xhr.send(data);
+                );
+
+                res.status(StatusCodes.CREATED).json({
+                    message: ReasonPhrases.CREATED,
+                });
+            } catch (error) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    message: ReasonPhrases.BAD_REQUEST,
+                });
+            }
+
+            // var url = `https://${soapConfig.host}:${soapConfig.port}/api/subscribe`;
+            // var xhr = new XMLHttpRequest();
+            // xhr.open("POST", url);
+            // xhr.setRequestHeader("Content-Type", "text/xml");
+            // xhr.onreadystatechange = function () {
+            //     if (xhr.readyState === XMLHttpRequest.DONE) {
+            //         if (xhr.status === 200) {
+            //             var response = xhr.responseText;
+            //             parseString(response, (err: any, result: any) => {
+            //                 const json = JSON.stringify(result, null, 4);
+            //                 console.log(json);
+            //                 res.status(StatusCodes.CREATED).json({
+            //                     message: ReasonPhrases.CREATED,
+            //                 });
+            //             });
+            //         } else {
+            //             res.status(StatusCodes.BAD_REQUEST).json({
+            //                 message: ReasonPhrases.BAD_REQUEST,
+            //             });
+            //         }
+            //     }
+            // };
+            // var data = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+            //     <Body>
+            //         <rejectSubscribe xmlns="http://service.binotify/">
+            //             <arg0 xmlns="">${creatorID}</arg0>
+            //             <arg1 xmlns="">${subscriberID}</arg1>
+            //         </rejectSubscribe>
+            //     </Body>
+            // </Envelope>`;
+            // xhr.send(data);
         };
     }
 }
